71efe46ff213558c9ccd4d1267a180e81d01bcb7
fernandohruz <fernandohruz@hotmail.com>
2024-03-23 01:35:37 -0600

    feat: calculate total exercises

--- part1/course-info/src/Total.jsx
+++ part1/course-info/src/Total.jsx
@@ -1,12 +1,8 @@
 const Total = (props) => {
-  return (
-    <p>
-      Number of exercises{" "}
-      {props.parts[0].exercises +
-        props.parts[1].exercises +
-        props.parts[2].exercises}
-    </p>
-  );
+  const getTotalExercises = (parts) => {
+    return parts.reduce((total, part) => total + part.exercises, 0);
+  };
+  return <p>Number of exercises {getTotalExercises(props.parts)}</p>;
 };
 
 export default Total;
